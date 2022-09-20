ac4d230d26f42071645b02bebe8d65235919251b
yoeltripp <114022435+Trippxzz@users.noreply.github.com>
2022-09-20 16:09:38 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -7,7 +7,7 @@ Config.Hide0 = false -- True to hide factions that have no players
 
 
 --DISCORD STATUS
-Config.MessageId = '1016502759895347300'
+Config.MessageId = 'COPY ID'
 Config.Debug = false
 Config.Use24hClock = true
 Config.UpdateTime = 1000*10*1 --10 SECONDS
